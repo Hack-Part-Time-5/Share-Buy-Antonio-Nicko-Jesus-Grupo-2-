c652add462ccd8757068af92f94ec60d56473606
antonioizq <antonio.izqui@gmail.com>
2023-02-28 21:24:43 +0100

    Login creado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,38 @@
 <x-layout>
-    
+    <x-slot name='title'>Rapido--Login</x-slot>
+    {{-- FORM LOGIN --}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                {{-- FORM TITLE --}}
+                <h2 class="form-title space-around">Login</h2>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{$error}}</li>   
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif 
+                {{-- FORM FIELDS --}}
+                <form action="/login" method="POST" class="form-control">
+                    @csrf
+                    {{-- EMAIL --}}
+                    <div class="space-around my-2">
+                        <input type="email" name="email" id="email" class="form-control forms_field-input" placeholder="Your email" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+                        <div class="validate"></div>
+                    </div>
+                    {{-- PASSWORD --}}
+                    <div class="space-around my-2">
+                        <input type="password" name="password" id="password" class="form-control forms_field-input" placeholder="Your password">
+                        <div class="validate"></div>
+                    </div>
+                       {{-- BUTTON-LOGIN --}}
+                       <button type="button" class="btn btn-info">Enter</button>
+                </form>
+                <p class="my-3">Have you an account?<a class="btn btn-info btn-sm ms-2" href="{{route('register')}}">Register</a></p>
+            </div>
+        </div>
+    </div>
 </x-layout>
