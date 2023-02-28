31fc94087356da5fd46fd80e96c8cc2ee1fa6fe1
Jesus Escalona <susophotos@gmail.com>
2023-03-01 00:28:36 +0100

    formulario de insercion de anuncio, errores corregidos

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Ad;
 use Livewire\Component;
+
 
 class CreateAd extends Component
 {
+
+    public $title;
+    public $body;
+    public $price;
+
+
+
+    protected $rules = [
+        'title'=>'requires|min:4',
+        'body'=>'required|min:8',
+        'price'=>'required|numeric'
+    ];
+    protected $messages = [
+        'requires'=>'Field :atrtibute is required, please fill it',
+        'min'=>'Field :attribute should be longer than :min',
+        'numeric'=>'Field :attribute must be a number'
+    ];
+    public function store()
+    {
+        Ad::create([
+            'title'=>$this->title,
+            'body'=>$this->body,
+            'price'=>$this->price
+        ]);
+
+        session()->flash('message', 'Ad created successfully');
+
+        $this->cleanForm();
+
+    }
+
+    public function update($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+    public function cleanForm()
+    {
+        $this->title = "";
+        $this->body = "";
+        $this->price = "";
+    }
     public function render()
     {
         return view('livewire.create-ad');
