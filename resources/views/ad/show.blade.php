aaecfa55eef06bc4e53be8aba139a2cf597257ad
Jesus Escalona <susophotos@gmail.com>
2023-03-15 02:23:06 +0100

    avance 3 de la vista show

--- resources/views/ad/show.blade.php
+++ resources/views/ad/show.blade.php
@@ -3,8 +3,8 @@
         <x-search/>
         <div class="container px-3">
             <div class="row my-5">
-                <div class="col-12 col-md-6">
-                    {{-- <div id="adImages" class="carousel slide" data-bs-ride="true">
+                <div class="col-12 col-md-6 px-4">
+                    <div id="adImages" class="carousel slide" data-bs-ride="true">
                         <div class="carrousel-indicators">
                             @for ($i = 0; $i < $ad->images()->count(); $i++)
                                 <button type="button" data-bs-target='#adImages' data-bs-slide-to="{{$i}}" class="@if($i == 0) active @endif" aria-current="true" aria-label="Slide {{$i + 1}}"></button>                            
@@ -29,16 +29,40 @@
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
-                    </div> --}}
+                    </div>
+                    <div class="container__fotos--showAd p-2 mb-3">
+                        {{-- <div class="container__fotosAdSmall">
+                            <div class="container__fotoSmall">
+
+                            </div>
+                            <div class="container__fotoSmall">
+
+                            </div>
+                            <div class="container__fotoSmall">
+
+                            </div>
+                        </div> --}}
+                        {{-- <div class="container__fotoBig">
+                            @foreach ($ad->images as $image )
+                                <div class="">
+                                    @if ($ad->images())
+                                        <img src="{{Storage::url($image->path)}}" alt="" class="d-block w-100">    
+                                    @else
+                                        <img src="https://picsum.photos/700/600?a" alt="" class="d-block w-100">
+                                    @endif                                    
+                                </div>                             
+                            @endforeach
+                        </div> --}}
+                    </div>
                 </div>
-                <div class="col-12 col-md-6 position-relative">
+                <div class="container__informacion--ad col-12 col-md-6 position-relative">
                     <div class="container__show--info my-2"><p class="titulo__ad">{{$ad->title}}</p></div>
                     <div class="container__show--info my-2"><strong class="precio">{{$ad->price}}€</strong></div>
                     <div class="container__show--info my-2">
                         <a href="{{route('category.ads',$ad->category)}}" class="btn btn-primary category__tag rounded-pill px-3 py-1 mx-1"><img src="{{$ad->category->img}}" alt="" class="me-1 category__icon--tag">{{__($ad->category->name)}}</a>
                     </div>
                     <div class="container__show--info my-2"><p>{{$ad->body}}</p></div>
-                    <div class="container__show--info my-2"><b>{{__('Publicado: ')}}</b>{{$ad->created_at->format('d/m/Y')}}</div>
+                    <div class="container__show--info my-2"><span>{{__('Fecha de publicacion: ')}}</span><b>{{$ad->created_at->format('d/m/Y')}}</b></div>
                     <div class="d-none  d-lg-flex container__show--info container__show--infoUser my-2">
                         <div class="mx-2"><div class="container__photo--user d-flex justify-content-center align-items-center text-white">
                             <span>Foto</span>
@@ -50,10 +74,31 @@
                     </div>                        
                     </div>
                     <div class="my-2 d-flex align-items-center">
-                        <i class="bi bi-truck me-2"></i>
-                        <span class="entrega">Entrega en 3-7 dias</span>
-                    </div>                    
+                        <b><i class="bi bi-truck me-2"></i></b>
+                        <span class="entrega"><b>Entrega en 3-7 dias</b></span>
+                    </div>
+                    <div class="container__compartelo d-flex align-items-center">
+                        <p>Compartelo</p>
+                        <img src="/img/icons/icons_RRSS/fb_icon.svg" alt="Facebook icon" class="icon__rrss--show mx-1">
+                        <img src="/img/icons/icons_RRSS/instagram_icon.svg" alt="instagram icon" class="icon__rrss--show mx-1">
+                        <img src="/img/icons/icons_RRSS/youtube_icon.svg" alt="youtube icon" class="icon__rrss--show mx-1">
+                    </div>
                     <div class="container__show--info my-2 d-flex justify-content-end"><a href="#" class="navbar-register_button btn px-3 btn__comprar">{{__('Comprar')}}</a></div>                    
+                </div>
+                <div class="container__protegemos--show col-12 col-md-6 px-4 py-2 mt-3">
+                    <div class="d-flex">
+                        <i class="bi bi-shield-check me-1 icon__escudo"></i>
+                        <p><b>Protegemos tus compras</b></p><br>
+                    </div>
+                    <div class="d-flex my-3">
+                        <i class="bi me-1 bi-check2"></i>
+                        <p class="p__proteccion">Garantia de reembolso si el producto no es como se describe.</p>
+                    </div>                    
+                    <div class="d-flex">
+                        <i class="bi me-1 bi-check2"></i>
+                        <p class="p__proteccion">El vendedor recibe el pago cuando confirmas que el envio ha ido bien.</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
