7f57fa6233e338fcc0a08e4be22619832ade9372
Nicko AE <nickyman.zero@gmail.com>
2023-03-15 21:57:25 +0100

    avances en el footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,36 +1,51 @@
 <footer>
 <div class="container-fluid d-flex justify-content-center flex-column p-5">
-    <div class="row row-cols-1 row-cols-sm-12 row-cols-md-5 py-5 my-5 border-top">
-      <div class="col col-sm-12">
+    <div class="row py-5 my-5 border-top">
+      <div class="col mb-3 p-2 gx-5">
         <img src="/img/icons/logo_footer.svg" alt="">
         <p class="nav-item mb-2">©Copyright 2023</p>
+        <div class="col mt-2"><h5 class="mb-2">Somos personas como tú</h5>
+        <p>“Si no lo usas, súbelo”. Bajo esta premisa, Share & Buy se ha convertido en una comunidad en la que cada día millones de personas compran y venden productos de segunda mano</div>
       </div>
-        <ul class="nav flex-column">
+        {{-- <ul class="nav flex-column">
           
-            <h5>Somos personas como tú</h5>
+            <h5 class="mb-2">Somos personas como tú</h5>
           <li class="nav-item mb-2"><p>“Si no lo usas, súbelo”. Bajo esta premisa, Share & Buy se ha convertido en una comunidad en la que cada día millones de personas compran y venden productos de segunda mano</li>
-        </ul>
+        </ul> --}}
       
   
-      <div class="col mb-3">
+      <div class="col mb-3 gx-5">
         <h5>Share&Buy</h5>
-        <ul class="nav flex-column my-3">
+        <ul class="nav flex-column my-3 ">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Conócenos</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cómo funciona</a></li>
-          <li class="nav-item mb-2"><a href="{{route('revisor.become')}}" class="btn btn-danger nav-link p-0 m text-muted">Trabaja con nosotros</a></li>
+          <li class="nav-item mb-2"><a href="{{route('revisor.become')}}" class="nav-link p-0 m text-muted">Trabaja con nosotros</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
         </ul>
-        <div>
-          <h5>Conéctate con nosotros</h5>
+        {{-- <div> --}}
+          <h5 class="mb-2">Conéctate con nosotros</h5>
           <div class="container__compartelo d-flex align-items-center" >
             <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
             <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt=""class="icon__rrss--footer mx-1">
             <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
           </div>
       </div>
-      
+      {{-- <div class="col mb-3">
+        <h5 class="mb-2">Conéctate con nosotros</h5>
+        <div class="container__compartelo d-flex align-items-center" >
+          <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
+          <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt=""class="icon__rrss--footer mx-1">
+          <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
+        </div> --}}
+      {{-- <div class="col mb-3">
+        <h5 class="mb-3">Conéctate con nosotros</h5>
+        <div class="container__compartelo d-flex align-items-center" >
+          <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
+          <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt=""class="icon__rrss--footer mx-1">
+          <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
+        </div> --}}
 
-      <div class="col mb-3">
+      <div class="col mb-2 mt-3 gx-5">
         <h5>Legal</h5>
         <ul class="nav flex-column my-3">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Aviso legal </a></li>
