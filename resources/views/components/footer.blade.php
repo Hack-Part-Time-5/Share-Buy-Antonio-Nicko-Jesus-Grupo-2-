24c9c4abdda3d688b04f19887a9598d7c7d9ec6c
Nicko AE <nickyman.zero@gmail.com>
2023-03-16 21:36:49 +0100

    apaño en el footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,43 +1,48 @@
 <footer>
-<div class="container-fluid d-flex justify-content-center flex-column p-5">
-    <div class="row py-5 my-5 border-top">
-      <div class="col mb-3 p-2 gx-5">
-        <img src="/img/icons/logo_footer.svg" alt="">
-        <p class="nav-item mb-2">©Copyright 2023</p>
-        <div class="col mt-2"><h5 class="mb-2">Somos personas como tú</h5>
-        <p>“Si no lo usas, súbelo”. Bajo esta premisa, Share & Buy se ha convertido en una comunidad en la que cada día millones de personas compran y venden productos de segunda mano</div>
-      </div>
-        {{-- <ul class="nav flex-column">
+    <div class="container-fluid d-flex justify-content-center flex-column p-5">
+        <div class="row py-5 my-5 border-top">
+            <div class="col mb-3 p-2 px-5">
+                <img src="/img/icons/logo_footer.svg" alt="">
+                <p class="nav-item mb-2">©Copyright 2023</p>
+                <div class="col mt-2">
+                    <h5 class="mb-2">Somos personas como tú</h5>
+                    <p class="d-inline-flex me-5">“Si no lo usas, súbelo”. Bajo esta premisa, Share & Buy se ha
+                        convertido en una comunidad en la que cada día millones de personas compran y venden productos
+                        de segunda mano
+                </div>
+            </div>
+            {{-- <ul class="nav flex-column">
           
             <h5 class="mb-2">Somos personas como tú</h5>
           <li class="nav-item mb-2"><p>“Si no lo usas, súbelo”. Bajo esta premisa, Share & Buy se ha convertido en una comunidad en la que cada día millones de personas compran y venden productos de segunda mano</li>
         </ul> --}}
-      
-  
-      <div class="col mb-3 gx-5">
-        <h5>Share&Buy</h5>
-        <ul class="nav flex-column my-3 ">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Conócenos</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cómo funciona</a></li>
-          <li class="nav-item mb-2"><a href="{{route('revisor.become')}}" class="nav-link p-0 m text-muted">Trabaja con nosotros</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-        </ul>
-        {{-- <div> --}}
-          <h5 class="mb-2">Conéctate con nosotros</h5>
-          <div class="container__compartelo d-flex align-items-center" >
-            <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
-            <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt=""class="icon__rrss--footer mx-1">
-            <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
-          </div>
-      </div>
-      {{-- <div class="col mb-3">
+
+
+            <div class="col mb-3 px-5">
+                <h5>Share&Buy</h5>
+                <ul class="nav flex-column my-3 ">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Conócenos</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cómo funciona</a></li>
+                    <li class="nav-item mb-2"><a href="{{route('revisor.become')}}"
+                            class="nav-link p-0 m text-muted">Trabaja con nosotros</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                </ul>
+                {{-- <div> --}}
+                <h5 class="mb-2">Conéctate con nosotros</h5>
+                <div class="container__compartelo d-flex align-items-center">
+                    <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
+                    <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt="" class="icon__rrss--footer mx-1">
+                    <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt="" class="icon__rrss--footer mx-1">
+                </div>
+            </div>
+            {{-- <div class="col mb-3">
         <h5 class="mb-2">Conéctate con nosotros</h5>
         <div class="container__compartelo d-flex align-items-center" >
           <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
           <img src="\img\icons\icons_RRSS\instagram_icon.svg" alt=""class="icon__rrss--footer mx-1">
           <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
         </div> --}}
-      {{-- <div class="col mb-3">
+            {{-- <div class="col mb-3">
         <h5 class="mb-3">Conéctate con nosotros</h5>
         <div class="container__compartelo d-flex align-items-center" >
           <img src="\img\icons\icons_RRSS\fb_icon.svg" alt="" class="icon__rrss--footer mx-1">
@@ -45,16 +50,27 @@
           <img src="\img\icons\icons_RRSS\youtube_icon.svg" alt=""class="icon__rrss--footer mx-1">
         </div> --}}
 
-      <div class="col mb-2 mt-3 gx-5">
-        <h5>Legal</h5>
-        <ul class="nav flex-column my-3">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Aviso legal </a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Condiciones de uso</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Política de privacidad</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
-        </ul>
-      </div>
-      <div>
+            <div class="col mb-2 mt-3 px-5">
+                <h5>Legal</h5>
+                <ul class="nav flex-column my-3">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Aviso legal </a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Condiciones de uso</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Política de privacidad</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
+                </ul>
+                <div>
+                    <h5>Nuestros métodos de pago</h5>
+                    <div class="container__compartelo d-flex align-items-center">
+                        <img src="\img\icons\iconos_pago\VISA_icon .svg" alt="" class="icon__pago--footer mx-1">
+                        <img src="\img\icons\iconos_pago\mastercard_icon  .svg" alt="" class="icon__pago--footer mx-1">
+                        <img src="\img\icons\iconos_pago\maestro_icon.svg" alt="" class="icon__pago--footer mx-1">
+                        <img src="\img\icons\iconos_pago\paypal_icon.svg" alt="" class="icon__pago--footer mx-1">
+
+                    </div>
+                </div>
+            </div>
+                {{-- <div>
         <h5>Nuestros métodos de pago</h5>
         <div class="container__compartelo d-flex align-items-center" >
           <img src="\img\icons\iconos_pago\VISA_icon .svg" alt="" class="icon__pago--footer mx-1">
@@ -62,8 +78,7 @@
           <img src="\img\icons\iconos_pago\maestro_icon.svg" alt="" class="icon__pago--footer mx-1">
           <img src="\img\icons\iconos_pago\paypal_icon.svg" alt="" class="icon__pago--footer mx-1">
           
+        </div> --}}
+            </div>
         </div>
-    </div>
-    </div>
-  </footer>
-  
+</footer>
