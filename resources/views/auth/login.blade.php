6bc0a4a1bd45e5df8cfefd7f9664b0e165cc0784
Jesus Escalona <susophotos@gmail.com>
2023-03-22 03:10:28 +0100

    pequeño cambio vista list

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <div class="container m-auto">
         <div class="container d-flex justify-content-center flex-column align-items-center my-5">
             <h2>Para poder ver el anuncio detalladamente debe iniciar sesion, o estar registrado.</h2>
-            <button class="mt-5 navbar-register_button text-white px-3 py-2 mb-5"><a class="nav-link active" aria-current="page" data-bs-toggle="modal" data-bs-target="#dialog1">{{__('Registrate')}}</a></button>                    
+            <button class="mt-5 navbar-register_button text-white px-3 py-2 mb-5"><a class="nav-link active" aria-current="page" data-bs-toggle="modal" data-bs-target="#dialog1">{{__('Registrate o inicia sesio')}}</a></button>                    
         </div>
     </div>
     
