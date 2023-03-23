27b2445fe2a9bf76c56390b0b716639b691be71b
antonioizq <antonio.izqui@gmail.com>
2023-03-23 09:38:30 +0100

    contacto

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,53 +1,52 @@
-<x-layout>
-    <x-slot name="title">Share&Buy - Register</x-slot>
-    <div class="container-fluid bg-accent vh-100">
-        <div class="row mb-5 pb-5">
-            <div class="col-12 col-md-8 ms-5">
-                <div class="d-flex flex-column align-items-center">
-                    <div class="form-content justify-content-center mb-5 pb-5">
-                        <div class="section-title">
-                            <h2 class="form-title space-around">
-                                Create account
-                            </h2>
-                        </div>
-                        @if($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors as $error)
-                                        <li>{{ $error }}</li>                                        
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <form action="/register" method="POST" role="form" class="php-email-form">
-                            @csrf
-                            <div class="form-field-edit form-field space-around my-2">
-                                <input type="text" name="name" id="name" class="form-control forms_field-input" placeholder="Your name" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                                <div class="validate"></div>                                
-                            </div>
-                            <div class="form-field-edit form-field space-around my-2">
-                                <input type="email" name="email" id="email" class="form-control forms_field-input" placeholder="Your email" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
-                                <div class="validate"></div>
-                            </div>
-                            <div class="form-field-edit form-field space-around my-2">
-                                <input type="password" name="password" id="password" class="form-control.forms_fields-input" placeholder="Your password">
-                                <div class="validate"></div>
-                            </div>
-                            <div class="form-field-edit form-field space-around my-2">
-                                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control.forms_fields-input" placeholder="Your password again">
-                                <div class="validate"></div>
-                            </div>
-                            <button type="submit" class="btn btn-info form-button-edit text-center space-around my-2">
-                                Create account
-                            </button>
-                        </form>    
-                    </div>
-                    <div class="form-link mt-4 d-flex">
-                        <p class="text-white">You are already one of us?</p>
-                        <a href="{{route('login')}}" class="text-reset text-decoration none ps-2">Come in now!</a>
-                    </div>
-                </div>
-            </div>
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-    </div>
-</x-layout>    
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ml-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,29 +1,107 @@
 <x-layout>
-    <div class="container">
-        <div class="containerTitleContact">
-            <img src="" alt="icono de contacto">
-            <h2>{{__('Contacta con nosotros')}}</h2>
-        </div>
-        <div class="containerFormContact container">
-            <div class="row">
-                <div class="col6 divFormContact">
-                    <form action="">
-                        <input type="text" name="name" placeholder="Nombre *">
-                        <input type="text" name="lastName" id="" placeholder="Apellidos *">
-                        <input type="email" name="email" id="" placeholder="Correo electronico *">
-                        <input type="tel" name="phone" id="" placeholder="Telefono *">
+    <div class="mt-4 container contact-page m-auto  d-flex">
+        <div class="row1">
+            <h2 class="title text-center">
+                <img src="\img\icons\contacta_icon.svg" class="contacta_icon"alt="icono de contacto">
+                {{ __('Contacta con nosotros') }}</h2>
 
-                        <p>*Campos obligatorios</p>
+            <form>
+            <div class=" mt-4 form-contact contact-page container d-flex justify-content-center m-auto">
+                <div class="row justify-content-center">
+                    <div class="col ">
+                       
+                            <input type="text" name="name" placeholder="Nombre *"
+                                class="placeholder_login form-control  p-2  ">
+                            </div>
+                            <div class="col">
+                            <input type="text" name="lastName" id="" placeholder="Apellidos *"
+                                class="placeholder_login form-control p-2  ">
+                            </div>
+                            <div>
+                            <div class="col">
+                            <input type="email" name="email" id="" placeholder="Correo electrónico *"
+                                class="placeholder_login form-control  p-2 ">
+                            </div>
+                                <div class="col">
+                                <input type="tel" name="phone" id="" placeholder="Teléfono *"
+                                class="placeholder_login form-control  p-2  ">
+                            </div>
+                        </div>
 
-                        <h5>¿Como te podemos ayudar?</h5>
-
-                        <textarea name="coments" id="" cols="30" rows="10"></textarea>
-                        <input type="checkbox" name="recaptcha" id="">No soy un robot
-                        <button type="submit" class="navbar-register_button text-white px-3">Enviar</button>
+                            <p class="text-required">* Campos obligatorios</p>
+                            <h5 >¿Cómo te podemos ayudar?</h5>
+                            <textarea name="coments" id="" cols="16" rows="8" class="form-control mt-4"></textarea>
+                            <div class="g-recaptcha" data-sitekey="6Lep6I8aAAAAACa6dzwxa9Q8jS1ahsnFO-mVjSby">
+                                <div>
+                                    <div><iframe title="reCAPTCHA"
+                                            src="https://www.google.com/recaptcha/api2/anchor?ar=2&amp;k=6Lep6I8aAAAAACa6dzwxa9Q8jS1ahsnFO-mVjSby&amp;co=aHR0cHM6Ly9pbnNweXJpYS5jb206NDQz&amp;hl=es&amp;v=Trd6gj1dhC_fx0ma_AWHc1me&amp;size=normal&amp;cb=2qgzz6mjs5k9"
+                                            width="304" height="78" role="presentation" name="a-u4e7alit1url"
+                                            frameborder="0" scrolling="no"
+                                            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe>
+                                    </div><textarea id="g-recaptcha-response-1" name="g-recaptcha-response"
+                                        class="g-recaptcha-response"
+                                        style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea>
+                                </div><iframe style="display: none;"></iframe>
+                            </div>
+                            <div>
+                                <button type="submit" class="navbar-register_button text-white px-3 mt-3">Enviar</button>
+                            </div>
+                            <div class="contact-policy mt-3">
+                                <p>Al hacer clic en Enviar, aceptas los <a target="_blank"
+                                        href="{{route('terminos')}}">Términos
+                                        y condiciones</a> de Share&Buy y nuestra <a target="_blank"
+                                        href="{{route('politicaprivacidad')}}">Política de privacidad</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                     </form>
-                    <h6>Al hacer clic en Enviar, aceptas los <SPan>Teminos y condiciones</SPan> de Share & Buy y nuestra <span>Politica de privacidad</span></h6>
                 </div>
-                <div class="col6 divContactInfo">
+                <div class="info-contact m-3 p-2 ">
+
+                    <img src="\img\contacto_img.png">
+
+                    <div class="white-box">
+                        <h4>Share&Buy cerca de ti</h4>
+
+                        <div class="line"></div>
+
+                        <div>
+                            <p class="title_contact mt-3 ">Correo electrónico</p>
+                            <p class="value mt-1">info@shareandbuy.com</p>
+
+                            <p class="title_contact mt-2 ">Teléfono</p>
+                            <p class="value mt-1">+34 630 10 10 16 <a rel="nofollow" target="_blank"
+                                    href="https://wa.me/+34660101016?text=hola%2C+estoy+interesad%40+en..."><img
+                                        alt="WhatsApp" title="WhatsApp" style="height: 19px;margin-bottom: -4px;"
+                                        src="\img\icons\whatsapp.png"></a></p>
+
+                            <p class="title_contact mt-2 ">Oficina</p>
+                            <p class="value mt-1">Barcelona, España</p>
+                        </div>
+
+                        <div class="m-3">
+                            
+                                <img alt="Facebook" title="Facebook" class="social-media"
+                                    src="\img\icons\icons_RRSS\fb_icon.svg">
+                            
+                            
+                                <img alt="Instagram" title="Instagram" class="social-media"
+                                    src="\img\icons\icons_RRSS\instagram_icon.svg">
+                           
+                           
+                                <img alt="YouTube" title="YouTube" class="social-media"
+                                    src="\img\icons\icons_RRSS\youtube_icon.svg">
+                           
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+                {{-- <div class="col6 divContactInfo">
                     <div>
                         <img src="" alt="imagen del carrito">
                     </div>
@@ -42,11 +120,13 @@
                             <img src="" alt="youtube">
                         </div>
                     </div>
-                    
+                  </div>   --}}
 
-                </div>
+
             </div>
 
         </div>
     </div>
-</x-layout>    
+    </div>
+
+</x-layout>
