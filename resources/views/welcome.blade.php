069bc5e6d8c2f4383b64a484a33766ffa6bef6c0
Jesus Escalona <susophotos@gmail.com>
2023-04-04 00:14:31 +0200

    retrocediendo la prueba hacer pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <div class="d-flex flex-column row justify-content-center">
             <div class="banner d-none d-lg-flex justify-content-center m-auto  align-items-center text-white">                                
                 <div class="d-flex flex-column flex-wrap">
-                    <h1 class="text-center mb-3"> {{__('no compres')}}</h1>
+                    <h1 class="text-center mb-3"> {{__('Compra y vende en un solo click')}}</h1>
                     <div class="d-flex flex-wrap justify-content-center position-relative container__buscador">                        
                         <form action="{{route('search')}}" method="GET" role="search">
                             <input type="search" placeholder=" {{__('Empieza tu búsqueda')}}" class=" buscador_banner placeholder_buscador mx-2" name="q" aria-label="Search">
